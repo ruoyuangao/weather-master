b6cb087873ffd104d55943a1eb4e27d70b43e18b
Kelly(Ruoyuan) Gao <89848323+ruoyuangao@users.noreply.github.com>
2023-01-24 21:51:00 -0600

    bug fix

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -1,17 +1,33 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
+@RefreshScope
 public class ConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
 
+    private static int port;
+
+    @Value("${server.port}")
+    public void setPort(int port){
+        this.port=port;
+    }
+
+    @RequestMapping("/port")
+    public int port(){
+        return port;
+    }
+
 }
-
-
